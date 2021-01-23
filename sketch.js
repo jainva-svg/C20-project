099fbfc9220e49b96e2d74d6e18bdf105071f3c4
jainva-svg <73653204+jainva-svg@users.noreply.github.com>
2021-01-23 12:15:17 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -14,7 +14,7 @@ function draw() {
   background("white");  
   if (wall.x-car.x<(car.width+wall.width)/2) {
     car.velocityX=0;
-  }
+  
     var deformation = 0.5* weight * speed* speed/22509;
     if (deformation>180) {
     
@@ -27,6 +27,7 @@ function draw() {
     if (deformation<100) {
       car.shapeColor=color(0,255,0);
     }
+  }
   
   drawSprites();
 }
